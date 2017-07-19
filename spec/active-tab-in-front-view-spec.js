821a45e20d8ad9872b744c1a0c8fa5abf074a58a
Michael Pontus <m.pontus@gmail.com>
2017-07-19 17:45:48 +0300

    Initial

---
+++ spec/active-tab-in-front-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ActiveTabInFrontView from '../lib/active-tab-in-front-view';
+
+describe('ActiveTabInFrontView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
